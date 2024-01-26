186ac811b58c28af0dd827c3d85af5a69193c012
plaze <banikakash131@gamil.com>
2024-01-26 20:34:23 +0530

    feat: setting cors

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import cors from "cors";
+import corsOptions from "./config/corsOptions.js";
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5555;
 const app = express();
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.send("hello");
+});
 
 app.listen(PORT, () => {
   console.log(`server is listening on Port ${PORT}`);
