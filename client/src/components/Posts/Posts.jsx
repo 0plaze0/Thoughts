d458636f894146bf1527b9f006cd13866d120f37
plaze <banikakash131@gamil.com>
2024-01-26 10:38:57 +0530

    feat(component): add posts

---
+++ client/src/components/Posts/Posts.jsx
@@ -0,0 +1,35 @@
+import "./Posts.scss";
+import { image } from "../../constants";
+const Posts = () => {
+  return (
+    <div className="app__posts">
+      {[1, 2, 3].map((item) => (
+        <div className="app__posts-container">
+          <div className="app__posts-img">
+            <img src={image.post1} alt="post" />
+          </div>
+          <div className="app__posts-text">
+            <h2>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam,
+              quibusdam!
+            </h2>
+            <div className="app__posts-info">
+              <a className="app__posts-author" href="#">
+                Lorem ipsum
+              </a>
+              <time>2024-01-24 22:36</time>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+              praesentium voluptatibus cumque tenetur. Facere commodi
+              architecto, vero sit, saepe cum ut ullam sunt qui, fuga dolores
+              quaerat velit illum iure!
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Posts;
--- client/src/constants/Images.jsx
+++ client/src/constants/Images.jsx
@@ -1,3 +1,5 @@
 import logo from "./../assets/logo.png";
+import post1 from "./../assets/posts(1).jpg";
+import post2 from "./../assets/posts(2).jpg";
 
-export default { logo };
+export default { logo, post1, post2 };
