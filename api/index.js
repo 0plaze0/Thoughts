402a2e2913efba2eeb754d7c027af724f0bb95a1
plaze <banikakash131@gamil.com>
2024-01-26 20:42:48 +0530

    feat: add register route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import corsOptions from "./config/corsOptions.js";
+import { router as register } from "./routes/register.js";
 
 const PORT = process.env.PORT || 5555;
 const app = express();
@@ -8,9 +9,7 @@ const app = express();
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.send("hello");
-});
+app.use("/register", register);
 
 app.listen(PORT, () => {
   console.log(`server is listening on Port ${PORT}`);
