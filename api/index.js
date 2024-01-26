0e1e5366b299418d53bb786f7f4f417c28353201
plaze <banikakash131@gamil.com>
2024-01-26 21:34:00 +0530

    feat: add database configuration

--- api/index.js
+++ api/index.js
@@ -1,16 +1,26 @@
+import "dotenv/config.js";
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+
+import connectDB from "./config/dbConn.js";
 import corsOptions from "./config/corsOptions.js";
 import { router as register } from "./routes/register.js";
 
+connectDB(process.env.DB_URI);
 const PORT = process.env.PORT || 5555;
 const app = express();
 
+//middleware
 app.use(cors(corsOptions));
 app.use(express.json());
 
+//routes
 app.use("/register", register);
 
-app.listen(PORT, () => {
-  console.log(`server is listening on Port ${PORT}`);
+mongoose.connection.once("open", () => {
+  console.log("connect to mongodb");
+  app.listen(PORT, () => {
+    console.log(`server is listening on Port ${PORT}`);
+  });
 });
