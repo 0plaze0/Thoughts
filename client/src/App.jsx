1ba989ccf9d9c967e425d06fcf990ec3ca2b3bfa
plaze <banikakash131@gamil.com>
2024-01-26 11:02:27 +0530

    feat: add page routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./App.scss";
 import { Navbar, Posts } from "./components";
-
+import Home from "./Layout/Home";
 function App() {
   return (
     <div className="app">
-      <Navbar />
-      <Posts />
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Posts />} />
+          <Route path="/login" element={<p>Login</p>} />
+          <Route path="/register" element={<p>Register</p>} />
+        </Route>
+      </Routes>
     </div>
   );
 }
