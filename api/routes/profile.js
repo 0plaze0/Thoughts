029f1cab46337ddefcae47f902423b7343c31ef2
plaze <banikakash131@gamil.com>
2024-01-27 23:58:55 +0530

    feat: add profile route

---
+++ api/routes/profile.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import jwt from "jsonwebtoken";
+import "dotenv/config.js";
+
+const router = Router();
+
+router.route("/").get(async (req, res) => {
+  const token = req.cookies["access-token"];
+  try {
+    const result = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    if (!result) return res.sendStatus(401);
+    return res.status(200).json(result);
+  } catch (err) {
+    return res.status(err.message);
+  }
+});
+
+export default router;
