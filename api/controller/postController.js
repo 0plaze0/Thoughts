40145601000fce8a4152694d7d5a6ed178cb6899
plaze <banikakash131@gamil.com>
2024-01-28 22:41:21 +0530

    merge stash

---
+++ api/controller/postController.js
@@ -0,0 +1,36 @@
+import Post from "./../model/postSchema.js";
+import fs from "fs";
+
+import { getExtension } from "../utils/getExtension.js";
+
+const createPost = async (req, res) => {
+  const { title, summary, content } = req.body;
+  const { originalname, path } = req.file;
+
+  const ext = getExtension(originalname);
+  const newPath = path + "." + ext;
+  fs.renameSync(path, newPath);
+  try {
+    const result = await Post.create({
+      title,
+      summary,
+      content,
+      cover: newPath,
+    });
+    return res.status(200).json(result);
+  } catch (err) {
+    return res.status(err.response.status).json(err.message);
+  }
+};
+
+const getAllPost = async (req, res) => {
+  try {
+    const result = await Post.find({});
+    if (!result) return res.status(200).json("no book in database");
+    return res.status(200).json(result);
+  } catch (err) {
+    return res.status(err.response.status).json(err.message);
+  }
+};
+
+export default { createPost, getAllPost };
