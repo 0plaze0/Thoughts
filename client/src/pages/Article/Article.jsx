161d63a5fd04ff0c1071805c8c3c092d95de53e0
plaze <banikakash131@gamil.com>
2024-01-29 23:36:16 +0530

    feat: add author info in article

--- client/src/pages/Article/Article.jsx
+++ client/src/pages/Article/Article.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { api } from "../../services/api";
+import { format } from "date-fns";
 import "./Article.scss";
 
 const Article = () => {
@@ -12,7 +13,9 @@ const Article = () => {
     const fetchData = async () => {
       try {
         const result = await api.get(`/post/${id}`);
-        if (result.status == 200) setData(result.data);
+        if (result.status == 200) {
+          setData(result.data);
+        }
       } catch (err) {
         console.log(err.message);
       }
@@ -28,7 +31,16 @@ const Article = () => {
         </div>
         <div className="app__article-text">
           <h2>{data.title}</h2>
+          <div className="app__article-info">
+            <p>{data?.author?.username}</p>
 
+            <time>
+              {format(
+                new Date(data?.createdAt || "1999-01-01 00:00"),
+                "dd-mm-yyyy HH:mm"
+              )}
+            </time>
+          </div>
           <div dangerouslySetInnerHTML={{ __html: data.content }} />
         </div>
       </div>
